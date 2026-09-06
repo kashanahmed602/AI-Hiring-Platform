047710133fdcabb1cbdb704887908392067a8695
Kashan Ahmed <kashanahmed3105@gmail.com>
2026-09-06 22:21:37 +0500

    Set up

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     try{
         await mongoose.connect(process.env.MONGODB_URI);
 
-        console.log('MongoDB connected successfully');
+        console.log('DataBase connected successfully');
     }catch(error){
 
         console.log(`Error connecting to MongoDB: ${error.message}`);
